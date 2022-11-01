5bf53f3c8b117848aa1daf90981602fc3f1f1ad4
josegabriels <jose.gabriels12@gmail.com>
2022-11-01 19:58:44 -0300

    constants roles file fixed

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,8 +1,7 @@
-export const ROLES = { 
-    ADMIN: 'admin', 
-    DONOR: 'donor',
-    ONG_OWNER: 'ong_owner',
-    CAMPAIGN_OWNER: 'campaign_owner',
-    MULTI_OWNER: 'multi_owner'
- }
- 
+export const ROLES = {
+  ADMIN: 'admin',
+  DONOR: 'donor',
+  ONG_OWNER: 'ong_owner',
+  CAMPAIGN_OWNER: 'campaign_owner',
+  MULTI_OWNER: 'multi_owner'
+}
