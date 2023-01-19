ccdc8db457f18214dd4b545922a64956ee49fe2a
josegabriels <jose.gabriels12@gmail.com>
2023-01-18 21:09:03 -0300

    added donor module and my-donations component

--- src/app/models/contribution.model.ts
+++ src/app/models/contribution.model.ts
@@ -1,10 +1,15 @@
+import { ItemDonation } from "./item-donation.model";
+
 export class Contribution {
 
     id: number;
-    item_donation_id: number;
     mount: number;
     status: number;
     code: string;
+    created_at: string;
+
+    item_donation: ItemDonation;
+    entity_name: string;
 
     constructor(data?:any){
         Object.assign(this, data)
